3453d6aa44d78d84a6423ec8f88e3fc1f5600b99
Brunno Novais <brunno.novais.77@gmail.com>
2020-04-14 19:44:52 -0300

    Update morse.py

--- morse.py
+++ morse.py
@@ -2,6 +2,40 @@ def lin():
     print('-' * 40)
 
 
+def continuar(frase):
+    texto1 = frase
+    texto = f'  {texto1}  '
+    texto = texto.replace(" .- ", "a")
+    texto = texto.replace(" -... ", "b")
+    texto = texto.replace(" -.-. ", "c")
+    texto = texto.replace(" -.. ", "d")
+    texto = texto.replace(" . ", "e")
+    texto = texto.replace(" ..-. ", "f")
+    texto = texto.replace(" --. ", "g")
+    texto = texto.replace(" .... ", "h")
+    texto = texto.replace(" .. ", "i")
+    texto = texto.replace(" .--- ", "j")
+    texto = texto.replace(" -.- ", "k")
+    texto = texto.replace(" .-.. ", "l")
+    texto = texto.replace(" -- ", "m")
+    texto = texto.replace(" -. ", "n")
+    texto = texto.replace(" --- ", "o")
+    texto = texto.replace(" .--. ", "p")
+    texto = texto.replace(" --.- ", "q")
+    texto = texto.replace(" .-. ", "r")
+    texto = texto.replace(" ... ", "s")
+    texto = texto.replace(" - ", "t")
+    texto = texto.replace("..- ", "u")
+    texto = texto.replace(" ...- ", "v")
+    texto = texto.replace(" .-- ", "w")
+    texto = texto.replace(" -..- ", "x")
+    texto = texto.replace(" -.-- ", "y")
+    texto = texto.replace(" --.. ", "z")
+    texto = texto.replace(" / ", " ")
+    texto.strip()
+    print(texto)
+
+
 def codificar():
     texto = input('Texto para morse: ')
     texto = texto.lower()
@@ -34,6 +68,7 @@ def codificar():
     texto = texto.replace("z", " --.. ")
     texto = texto.strip()
     print(texto)
+    return texto
 
 
 def decodificar():
@@ -80,7 +115,14 @@ while True:
     lin()
 
     if x == 1:
-        codificar()
+        frase = codificar()
+        while True:
+            escolha = str(input('Deseja Decodificar o Código? [S / N] \n> ')).lower().strip()[0]
+            if escolha in 's':
+                continuar(frase=frase)
+                break
+            elif escolha in 'n':
+                break
         break
     elif x == 2:
         decodificar()
